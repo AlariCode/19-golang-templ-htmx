9aadfc62f0e0f660709f5dd1995ffb76692cef03
Alaricode <antonlarichev@gmail.com>
2025-01-29 18:05:30 +0300

    Упражнение - Имитация входа

--- views/widgets/login-form_templ.go
+++ views/widgets/login-form_templ.go
@@ -35,7 +35,7 @@ func LoginForm() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<form hx-post=\"/\" hx-trigger=\"submit\" class=\"login-form\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"login-result\"></div><form hx-ext=\"response-targets\" hx-post=\"/api/login\" hx-trigger=\"submit\" class=\"login-form\" hx-target=\"#login-result\" hx-target-error=\"#login-result\" hx-swap=\"innerHTML\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -49,6 +49,7 @@ func LoginForm() templ.Component {
 		templ_7745c5c3_Err = components.Input(components.InputProps{
 			Name:        "password",
 			Placeholder: "Пароль",
+			Type:        "password",
 		}).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
@@ -104,7 +105,7 @@ func LoginFormStyle() templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<style>\n\t.login-form {\n\t\tdisplay: flex;\n\t\tflex-direction: column;\n\t\tgap: 30px;\n\t\twidth: 100%;\n\t\tmax-width: 520px;\n\t}\n\t</style>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<style>\n\t#login-result {\n\t\tmargin-bottom: 10px;\n\t}\n\t.login-form {\n\t\tdisplay: flex;\n\t\tflex-direction: column;\n\t\tgap: 30px;\n\t\twidth: 100%;\n\t\tmax-width: 520px;\n\t}\n\t</style>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
