96151f7176a047db76f9ee6461bdbcc715e6bd6e
Alaricode <antonlarichev@gmail.com>
2025-01-29 18:31:38 +0300

    Упражнение - Выход пользователя

--- views/layout/menu_templ.go
+++ views/layout/menu_templ.go
@@ -59,7 +59,7 @@ func Menu() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span> <a class=\"menu__right-link\" href=\"/login\">Выйти</a>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span> <a class=\"menu__right-link\" hx-get=\"/api/logout\" hx-trigger=\"click\">Выйти</a>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
